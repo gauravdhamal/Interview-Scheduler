b06270291abbc81b6e43df9823396aeaf838d92a
gauravdhamal <gauravdhamal11@gmail.com>
2023-02-22 17:11:19 +0530

    Recruiter Service done

--- Interview_Scheduler/src/main/java/com/naukari/services/RecruiterService.java
+++ Interview_Scheduler/src/main/java/com/naukari/services/RecruiterService.java
@@ -1,5 +1,27 @@
 package com.naukari.services;
 
+import java.util.List;
+
+import com.naukari.exception.RecordNotFoundException;
+import com.naukari.models.Candidate;
+import com.naukari.models.Interviewer;
+import com.naukari.models.Recruiter;
+
 public interface RecruiterService {
+
+	public Recruiter createRecruiter(Recruiter recruiter);
+
+	public Recruiter updateRecruiter(Integer recruitrtId, Recruiter recruiter) throws RecordNotFoundException;
+
+	public Recruiter getRecruiter(Integer recruiterId) throws RecordNotFoundException;
+
+	public String deleteRecruiter(Integer recruiterId) throws RecordNotFoundException;
+
+	public List<Interviewer> getAllInterviewers(Integer recruiterId) throws RecordNotFoundException;
+
+	public List<Candidate> getAllCandidates(Integer recruiterId) throws RecordNotFoundException;
+
+	public String scheduleInterview(Integer recruiterId, Integer candidateId, Integer interviewerId)
+			throws RecordNotFoundException;
 
 }
