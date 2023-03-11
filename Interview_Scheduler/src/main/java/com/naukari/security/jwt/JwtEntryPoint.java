81de0093a52c0073b74f17c82eb461d3cb8b3cd2
gauravdhamal <gauravdhamal11@gmail.com>
2023-03-11 15:34:27 +0530

    JwtEntryPoint Implemented.

---
+++ Interview_Scheduler/src/main/java/com/naukari/security/jwt/JwtEntryPoint.java
@@ -0,0 +1,40 @@
+package com.naukari.security.jwt;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// It is like entry check.
+@Component
+public class JwtEntryPoint implements AuthenticationEntryPoint {
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException, ServletException {
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+		Map<String, Object> body = new HashMap<>();
+		body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+		body.put("error", "Unauthorized");
+		body.put("message", authException.getMessage());
+		body.put("path", request.getServletPath());
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.writeValue(response.getOutputStream(), body);
+
+		System.out.println("Inside JwtEntryPoint");
+	}
+
+}
